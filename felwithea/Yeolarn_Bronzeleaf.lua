6a430966610739f13adf556b3f3c921708427f34
Nick <nmargalis@gmail.com>
2018-05-03 17:44:43 -0700

    Update felwithea quest files

--- felwithea/Yeolarn_Bronzeleaf.lua
+++ felwithea/Yeolarn_Bronzeleaf.lua
@@ -22,12 +22,12 @@ function event_say(e)
 	elseif(e.message:findi("warden of tunare")) then
 		e.self:Say("The crate that the you recovered from the Courier contained this black candlestick that radiates an aura of great magical power. I request that you deliver the candlestick to Lady Trilani who is studying with the High Men in Erudin. Perhaps she can divine the nature of the candlestick and offer aid in defeating its power. The remaining contents of the crate have been given to Crusader Swiftmoon to be delivered to a gnome in steamfont that is purchasing them on behalf of the Eldrich Collective. When you return be sure to present your Disciple Symbol to me with anything that Trilani may ask you to deliver.");
 		e.other:SummonItem(1598); -- Black Stone Candlestick
-	elseif(e.message:findi("trades")) then
-		e.self:Say("I thought you might be one who was interested in the various different trades, but which one would suit you? Ahh, alas, it would be better to let you decide for yourself, perhaps you would even like to master them all! That would be quite a feat. Well, lets not get ahead of ourselves, here, take this book. When you have finished reading it, ask me for the [second book], and I shall give it to you. Inside them you will find the most basic recipes for each trade. These recipes are typically used as a base for more advanced crafting, for instance, if you wished to be a smith, one would need to find some ore and smelt it into something usable. Good luck!");
-		e.other:SummonItem(51121);
-	elseif(e.message:findi("second book")) then
-		e.self:Say("Here is the second volume of the book you requested, may it serve you well!");
-		e.other:SummonItem(51122);
+	-- elseif(e.message:findi("trades")) then
+	-- 	e.self:Say("I thought you might be one who was interested in the various different trades, but which one would suit you? Ahh, alas, it would be better to let you decide for yourself, perhaps you would even like to master them all! That would be quite a feat. Well, lets not get ahead of ourselves, here, take this book. When you have finished reading it, ask me for the [second book], and I shall give it to you. Inside them you will find the most basic recipes for each trade. These recipes are typically used as a base for more advanced crafting, for instance, if you wished to be a smith, one would need to find some ore and smelt it into something usable. Good luck!");
+	-- 	e.other:SummonItem(51121);
+	-- elseif(e.message:findi("second book")) then
+	-- 	e.self:Say("Here is the second volume of the book you requested, may it serve you well!");
+	-- 	e.other:SummonItem(51122);
 	end
 end
 
